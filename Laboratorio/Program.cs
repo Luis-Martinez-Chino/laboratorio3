d49b32c04b3ba4fe3b5f3b998f6df4e76034f066
Luis-Martinez-Chino <luispmb22@gmail.com>
2026-02-11 07:50:34 -0600

    Update Program.cs

--- Laboratorio/Program.cs
+++ Laboratorio/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Luis Martinez - 1081126");
 Console.WriteLine("Laboratorio # 03");
 // Ingresar aquí nombre y carnet del dueño del repositorio
-
+Luis Martinez - 1081126
 // Ingresar aquí nombre y carnet del colaborador:
 
 Console.WriteLine("==========================================");
